9fa27d8f7d7813b68524282b2636d949fdfff5da
Trophy <jonahsantiago1000@gmail.com>
2025-10-27 15:58:47 -0400

    Began work on sample DriveSubsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/DriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/DriveSubsystem.java
@@ -1,6 +1,21 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
+import dev.nextftc.control.ControlSystem;
 import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.hardware.impl.MotorEx;
 
 public class DriveSubsystem implements Subsystem {
+    public static final DriveSubsystem INSTANCE = new DriveSubsystem();
+
+    private DriveSubsystem() { }
+
+    private MotorEx frontLeftMotor = new MotorEx("frontLeftMotor");
+    private MotorEx frontRightMotor = new MotorEx("frontRightMotor");
+    private MotorEx backLeftMotor = new MotorEx("backLeftMotor");
+    private MotorEx backRightMotor = new MotorEx("backRightMotor");
+
+    private ControlSystem driveControlSystem = ControlSystem.builder()
+            .posPid(0,0,0) // figure out what this is
+            .build();
+
 }
