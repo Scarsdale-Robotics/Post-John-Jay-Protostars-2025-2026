1b33d8a4a5cc64ee3239c3d3fed949dcb6c7d855
grek2 <madisonwharton0@gmail.com>
2025-11-04 20:49:44 -0500

    brake command

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/DriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/DriveSubsystem.java
@@ -3,7 +3,9 @@ package org.firstinspires.ftc.teamcode.Subsystems;
 import dev.nextftc.control.ControlSystem;
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.hardware.controllable.MotorGroup;
 import dev.nextftc.hardware.impl.MotorEx;
+import dev.nextftc.hardware.powerable.SetPower;
 
 public class DriveSubsystem implements Subsystem {
     public static final DriveSubsystem INSTANCE = new DriveSubsystem();
@@ -15,6 +17,8 @@ public class DriveSubsystem implements Subsystem {
     private MotorEx backLeftMotor = new MotorEx("backLeftMotor");
     private MotorEx backRightMotor = new MotorEx("backRightMotor");
 
+    private MotorGroup allWheels = new MotorGroup(frontLeftMotor, frontRightMotor, backLeftMotor, backRightMotor);
+
     private ControlSystem driveControlSystem = ControlSystem.builder()
             .posPid(0,0,0) // figure out what this is
             .build();
@@ -22,8 +26,7 @@ public class DriveSubsystem implements Subsystem {
     @Override
     public void periodic() {
 
-
-
+    //i forgor
     }
 
     public class driveRobotCentric extends Command{
@@ -39,6 +42,7 @@ public class DriveSubsystem implements Subsystem {
             return false;
         }
     }
+    public Command brake = new SetPower(allWheels, 0);
 
 }
 
