0a9436d5db22b04f60b81b6894068bfeabb51ebf
Trophy <jonahsantiago1000@gmail.com>
2025-12-01 15:43:26 -0500

    good ol' home-baked FieldCentric() like grandma used to make

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/DriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/DriveSubsystem.java
@@ -5,11 +5,37 @@ import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.commands.utility.InstantCommand;
 import dev.nextftc.core.subsystems.Subsystem;
 import dev.nextftc.hardware.controllable.MotorGroup;
+import dev.nextftc.hardware.driving.FieldCentric;
+import dev.nextftc.hardware.driving.MecanumDriverControlled;
 import dev.nextftc.hardware.impl.Direction;
 import dev.nextftc.hardware.impl.IMUEx;
 import dev.nextftc.hardware.impl.MotorEx;
 import dev.nextftc.hardware.powerable.SetPower;
 
+/*
+* DriveFieldCentric
+*Drive subsystem specifications:
+
+Required methods:
+
+Driverobotcentricpower(double strafepower,forwardpower,turnpower)
+
+Drivefieldcentric(theta, speed, turnpower, heading)
+
+Required commands:
+Brake
+
+drivefieldcentric:
+Theta: direction of drive, in radians.
+Speed: driving speed, in/sec
+Turn: turn speed, rad/sec
+Heading: robots current heading.
+
+Basically youll calculate how fast the robot would move if you went full speed in the direction,
+then use desired speed to get the actual speed needed. You can then like add turn.
+
+*
+* */
 public class DriveSubsystem implements Subsystem {
     public static final DriveSubsystem INSTANCE = new DriveSubsystem();
 
@@ -34,13 +60,35 @@ public class DriveSubsystem implements Subsystem {
     //i forgor
     }
 
-    public Command driveRobotCentric(double x, double y, double h) {
-        return new InstantCommand(() -> {
+    /**
+     *
+     * @param theta is the theta thing
+     * @param power
+     * @param turn turn
+     * @param heading help
+     * @return nothing (haskell hates this one weird trick)
+     */
+    public void driveFieldCentric(double theta, double speed, double turn, double heading) {
+        Command autoControlled = new MecanumDriverControlled(
+                frontLeftMotor,
+                frontRightMotor,
+                backLeftMotor,
+                backRightMotor,
+                // takes in four values, uses setPower() to send to motors,
+                new FieldCentric(imu)
+        );
+        autoControlled.schedule();
+
+
+
+    };
+
+    public void driveRobotCentric(double x, double y, double h) {
             frontLeftMotor.setPower(y + x + h);
             frontRightMotor.setPower(y - x - h);
             backLeftMotor.setPower(y - x + h);
             backRightMotor.setPower(y + x - h);
-        });
+        };
     }
 
     public Command brake = new SetPower(allWheels, 0);
