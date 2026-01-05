abf642b942483818620d0093049bc8bdf609232f
Nikolas McMahon <nikmc7877@gmail.com>
2026-01-05 16:10:43 -0500

    beginning to add pid

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/Auto.java
@@ -1,30 +1,20 @@
 package org.firstinspires.ftc.teamcode.Auton;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.teamcode.Subsystems.ShooterSubsystem;
 import org.firstinspires.ftc.teamcode.Subsystems.DriveSubsystem;
 import org.firstinspires.ftc.teamcode.Subsystems.LocalizationSubsystem;
 
 import dev.nextftc.control.ControlSystem;
 import dev.nextftc.control.KineticState;
-import dev.nextftc.control.feedback.FeedbackElement;
 import dev.nextftc.core.commands.Command;
-import dev.nextftc.core.commands.CommandManager;
 import dev.nextftc.core.commands.delays.Delay;
 import dev.nextftc.core.commands.groups.SequentialGroup;
 import dev.nextftc.core.components.SubsystemComponent;
-import dev.nextftc.ftc.Gamepads;
 import dev.nextftc.ftc.NextFTCOpMode;
 import dev.nextftc.ftc.components.BulkReadComponent;
 import dev.nextftc.hardware.driving.Drivetrain;
-import dev.nextftc.hardware.driving.FieldCentric;
-import dev.nextftc.hardware.driving.MecanumDriverControlled;
-import dev.nextftc.hardware.impl.Direction;
-import dev.nextftc.hardware.impl.IMUEx;
-import dev.nextftc.hardware.impl.MotorEx;
 
 @Autonomous(name="Autonomous Program")
 public class Auto extends NextFTCOpMode {
@@ -81,7 +71,7 @@ public class Auto extends NextFTCOpMode {
             LocalizationSubsystem.INSTANCE.setOdom(0,0,0),
             driveToPosRoboCentric(5,4,0),
             //DriveSubsystem.INSTANCE.brake,
-            ShooterSubsystem.INSTANCE.shooterOn(50),
+            ShooterSubsystem.INSTANCE.shooterSetTarget(50),
             new Delay(1),
             ShooterSubsystem.INSTANCE.shooterOff
         );
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ShooterSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ShooterSubsystem.java
@@ -1,13 +1,8 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
-import dev.nextftc.control.ControlSystem;
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.core.commands.utility.InstantCommand;
 import dev.nextftc.core.subsystems.Subsystem;
-import dev.nextftc.hardware.controllable.MotorGroup;
-import dev.nextftc.hardware.controllable.RunToPosition;
-import dev.nextftc.hardware.controllable.RunToState;
-import dev.nextftc.hardware.controllable.RunToVelocity;
 import dev.nextftc.hardware.impl.MotorEx;
 import dev.nextftc.hardware.powerable.SetPower;
 
@@ -15,26 +10,20 @@ public class ShooterSubsystem implements Subsystem {
     public static double shooterPower = 0;
     public static final ShooterSubsystem INSTANCE = new ShooterSubsystem(shooterPower);
     private ShooterSubsystem(double shooterPower) { }
-    private MotorEx motor1 = new MotorEx("flywheelMotor_1");
-    private MotorEx motor2 = new MotorEx("flywheelMotor_2");
-    private MotorGroup shooterGroup = new MotorGroup(motor1, motor2);
+    private MotorEx motor = new MotorEx("shooterMotor_1");
 
-        private ControlSystem controlSystem = ControlSystem.builder()
-                .posPid(0,0,0)
-                .build();
 
         @Override
-        public void periodic() {
-            shooterGroup.setPower(controlSystem.calculate(motor1.getState()));
+        public void periodic() { // put pid here
         }
 
-        public Command shooterOn(double shooterPower){
+        public Command shooterSetTarget(double shooterPower){
             return new InstantCommand(() -> {
-                shooterGroup.setPower(shooterPower);
+                motor.setPower(shooterPower);
             });
         }
 
         //single Command needed if no parameter input
-        public Command shooterOff = new SetPower(shooterGroup, 0).requires(this);
+        public Command shooterOff = new SetPower(motor, 0).requires(this);
 
 }
