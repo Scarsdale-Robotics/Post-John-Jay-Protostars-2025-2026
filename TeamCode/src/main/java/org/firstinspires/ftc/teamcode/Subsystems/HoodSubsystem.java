63dd94898218bb70fbdf40da2bcf217ca2f4e8ad
Nikolas McMahon <nikmc7877@gmail.com>
2026-01-07 15:52:20 -0500

    added hood subsystem its not done at all

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/HoodSubsystem.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.utility.InstantCommand;
+import dev.nextftc.hardware.impl.MotorEx;
+import dev.nextftc.hardware.impl.ServoEx;
+import dev.nextftc.hardware.powerable.SetPower;
+
+public class HoodSubsystem { // Not finished lots of stuff
+    //public static final HoodSubsystem INSTANCE = new HoodSubsystem();
+    private HoodSubsystem() { }
+    private ServoEx servo = new ServoEx("hoodServo");
+
+    public Command turn(double direction){
+        return new InstantCommand(() -> {
+            servo.setPosition(direction);
+        });
+    }
+
+    //single Command needed if no parameter input
+    //public Command shooterOff = new SetPower(motor, 0).requires(this);
+
+}
