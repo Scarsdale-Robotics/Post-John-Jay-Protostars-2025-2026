4032e093c0f145d4260a9fe4ee74c5071dbd3512
grek2 <madisonwharton0@gmail.com>
2026-01-12 14:40:42 -0500

    asdfjkl;

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/PusherSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/PusherSubsystem.java
@@ -7,8 +7,9 @@ import dev.nextftc.hardware.positionable.SetPosition;
 
 public class PusherSubsystem implements Subsystem {
     public static PusherSubsystem INSTANCE = new PusherSubsystem();
-
     public ServoEx pusherServo = new ServoEx("pusher_servo");
+    public double upPosition = 2;
+    public double downPosition = 1;
     public Command pusherUp = new SetPosition(pusherServo, upPosition);
     public Command pusherDown = new SetPosition(pusherServo, downPosition);
 
